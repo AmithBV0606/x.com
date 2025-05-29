6e3307c853d777ca073df317fd9f826237a33175
Amith B V <amithrao0606@gmail.com>
2025-05-29 08:40:51 +0530

    Basic layout setup for left-sidebar component.

--- x-client/app/page.tsx
+++ x-client/app/page.tsx
@@ -1,9 +1,13 @@
+import LeftSidebar from "@/components/left-sidebar";
+
 export default function Home() {
   return (
     <div className="container mx-auto h-screen">
       <div className="grid grid-cols-12 w-full h-screen">
         {/* Left Sidebar */}
-        <div className="col-span-3">Hello</div>
+        <div className="col-span-3">
+          <LeftSidebar />
+        </div>
 
         {/* Center Part */}
         <div className="col-span-6 border border-x-[1.5px] border-y-0 border-gray-600">
---
+++ x-client/components/left-sidebar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function LeftSidebar() {
+  return (
+    <div>
+      {/* Logo Icon : */}
+      <div className="mt-1 h-fit py-3 px-3 hover:bg-slate-900 rounded-full cursor-pointer w-fit transition-all"></div>
+
+      {/* Sidebar Menu : */}
+      <div></div>
+
+      {/* Mini Account display : */}
+      <div></div>
+    </div>
+  );
+}
