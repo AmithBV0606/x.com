2957623729f85e2ae52a4691daf20fb1922b10d6
Amith B V <amithrao0606@gmail.com>
2025-05-29 08:41:42 +0530

    Added logo for x.com using react-icons.

--- x-client/components/left-sidebar.tsx
+++ x-client/components/left-sidebar.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { FaXTwitter } from "react-icons/fa6";
 
 export default function LeftSidebar() {
   return (
     <div>
       {/* Logo Icon : */}
-      <div className="mt-1 h-fit py-3 px-3 hover:bg-slate-900 rounded-full cursor-pointer w-fit transition-all"></div>
+      <div className="mt-1 h-fit py-3 px-3 hover:bg-slate-900 rounded-full cursor-pointer w-fit transition-all">
+        <FaXTwitter size={38} className="" />
+      </div>
 
       {/* Sidebar Menu : */}
       <div></div>
