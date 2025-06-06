c16a869c6c82a6d059003f79761f00cb77eddc61
Amith B V <amithrao0606@gmail.com>
2025-06-06 17:32:06 +0530

    Added logic to send the google-OAuth data to backend.

--- x-server/src/app/index.ts
+++ x-server/src/app/index.ts
@@ -2,23 +2,42 @@ import express from "express";
 import bodyParser from "body-parser";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@as-integrations/express5";
+import { User } from "./users";
 
 export async function initServer() {
   const app = express();
 
   app.use(bodyParser.json());
 
+  // Graphql DEMO :
+
+  // const graphqlServer = new ApolloServer({
+  //   typeDefs: `
+  //     type Query {
+  //       sayHello: String
+  //       greet(name: String!): String
+  //     }
+  //   `,
+  //   resolvers: {
+  //     Query: {
+  //       sayHello: () => "Hello from Graphql server!!!",
+  //       greet: (parent, { name }: { name: string }) => `Good morning ${name}`
+  //     },
+  //   },
+  // });
+
   const graphqlServer = new ApolloServer({
-    typeDefs: `
+    typeDefs: `#graphql
+
+      ${User.types}
+
       type Query {
-        sayHello: String
-        greet(name: String!): String
+        ${User.queries}
       }
     `,
     resolvers: {
       Query: {
-        sayHello: () => "Hello from Graphql server!!!",
-        greet: (parent, { name }: { name: string }) => `Good morning ${name}`
+        ...User.resolvers.queries,
       },
     },
   });
