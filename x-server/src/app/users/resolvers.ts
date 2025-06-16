417b5726bb45af80610e6e04de96b0a677e8b6d3
Amith B V <amithrao0606@gmail.com>
2025-06-16 20:15:23 +0530

    Prisma logs

--- x-server/src/app/users/resolvers.ts
+++ x-server/src/app/users/resolvers.ts
@@ -7,7 +7,7 @@ const queries = {
   verifyGoogleToken: async (parent: any, { token }: { token: string }) => {
     const googleToken = token;
     const googleOauthURL = new URL("https://oauth2.googleapis.com/tokeninfo");
-    googleOauthURL.searchParams.set("id_token", token);
+    googleOauthURL.searchParams.set("id_token", googleToken);
 
     const { data } = await axios.get<GoogleTokenResult>(
       googleOauthURL.toString(),
